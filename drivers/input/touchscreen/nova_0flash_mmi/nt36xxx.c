e04793992b384abfa1da29f43fa2608c136a73a6
dianlujitao <dianlujitao@lineageos.org>
2022-02-01 13:54:41 +0800

    nova_0flash_mmi: Remove unused screen_state and mutex
    Change-Id: Ibb245928e04bdbdc71a6fbaad8e67da3405648b8

--- drivers/input/touchscreen/nova_0flash_mmi/nt36xxx.c
+++ drivers/input/touchscreen/nova_0flash_mmi/nt36xxx.c
@@ -2178,7 +2178,6 @@ static int nvt_sensor_set_enable(struct sensors_classdev *sensors_cdev,
 		unsigned int enable)
 {
 	NVT_LOG("Gesture set enable %d!", enable);
-	mutex_lock(&ts->state_mutex);
 	if (enable == 1) {
 		toggle_gesture(GESTURE_DOUBLE_CLICK, true);
 	} else if (enable == 0) {
@@ -2186,7 +2185,6 @@ static int nvt_sensor_set_enable(struct sensors_classdev *sensors_cdev,
 	} else {
 		NVT_LOG("unknown enable symbol\n");
 	}
-	mutex_unlock(&ts->state_mutex);
 	return 0;
 }
 
@@ -2960,12 +2958,6 @@ static int32_t nvt_ts_probe(struct spi_device *client)
 	mutex_init(&ts->lock);
 	mutex_init(&ts->xbuf_lock);
 
-#ifdef NVT_SENSOR_EN
-	mutex_init(&ts->state_mutex);
-	//unknown screen state
-	ts->screen_state = SCREEN_UNKNOWN;
-#endif
-
 	//---eng reset before TP_RESX high
 	nvt_eng_reset();
 
@@ -3663,14 +3655,7 @@ int32_t nvt_ts_suspend(struct device *dev)
 {
 	uint8_t buf[4] = {0};
 
-#ifdef NVT_SENSOR_EN
-	mutex_lock(&ts->state_mutex);
-#endif
-
 	if (!ts->bTouchIsAwake) {
-#ifdef NVT_SENSOR_EN
-		mutex_unlock(&ts->state_mutex);
-#endif
 		NVT_LOG("Touch is already suspend\n");
 		return 0;
 	}
@@ -3733,10 +3718,6 @@ int32_t nvt_ts_suspend(struct device *dev)
 	msleep(50);
 
 	NVT_LOG("end\n");
-#ifdef NVT_SENSOR_EN
-	ts->screen_state = SCREEN_OFF;
-	mutex_unlock(&ts->state_mutex);
-#endif
 
 	return 0;
 }
@@ -3750,14 +3731,7 @@ return:
 *******************************************************/
 int32_t nvt_ts_resume(struct device *dev)
 {
-
-#ifdef NVT_SENSOR_EN
-	mutex_lock(&ts->state_mutex);
-#endif
 	if (ts->bTouchIsAwake) {
-#ifdef NVT_SENSOR_EN
-		mutex_unlock(&ts->state_mutex);
-#endif
 		NVT_LOG("Touch is already resume\n");
 		return 0;
 	}
@@ -3831,10 +3805,6 @@ int32_t nvt_ts_resume(struct device *dev)
 
 	NVT_LOG("end\n");
 
-#ifdef NVT_SENSOR_EN
-	ts->screen_state = SCREEN_ON;
-	mutex_unlock(&ts->state_mutex);
-#endif
 	return 0;
 }
 
