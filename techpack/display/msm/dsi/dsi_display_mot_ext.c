moto-sm7250/android_kernel_motorola_sm7250

techpack: display: Add ACL and CABC sysfs nodes
Change-Id: Ie6cdaf4b78cf8f110d0e8cb79af301b0d5a08948

techpack/display/msm/dsi/dsi_display_mot_ext.c
@@ -1198,6 +1198,92 @@ static ssize_t dsi_display_hbm_set(struct device *dev,
 	return count;
 }
 
+static ssize_t dsi_display_acl_get(struct device *dev,
+	struct device_attribute *attr, char *buf)
+{
+	struct dsi_display *display;
+	display = dev_get_drvdata(dev);
+	if (!display) {
+		pr_err("Invalid display\n");
+		return snprintf(buf, PAGE_SIZE, "%d\n", 0);
+	}
+
+	return snprintf(buf, PAGE_SIZE, "%d\n", display->panel->acl_state);
+}
+
+static ssize_t dsi_display_acl_set(struct device *dev,
+	struct device_attribute *attr, const char *buf, size_t count)
+{
+	struct dsi_display *display;
+	struct msm_param_info param_info;
+	u16 val = 0;
+
+	display = dev_get_drvdata(dev);
+	if (!display) {
+		pr_err("Invalid display\n");
+		return count;
+	}
+
+	if (kstrtou16(buf, 10, &val) < 0)
+		return count;
+
+	param_info.value = val ? ACL_ON_STATE : ACL_OFF_STATE;
+	param_info.param_idx = PARAM_ACL_ID;
+	param_info.param_conn_idx = CONNECTOR_PROP_ACL;
+	display->panel->acl_state = param_info.value;
+
+	if (!dsi_panel_initialized(display->panel))
+		return count;
+
+	if (dsi_panel_set_param(display->panel, &param_info) < 0)
+		pr_err("Failed to set panel parameter id: %d\n", param_info.param_idx);
+
+	return count;
+}
+
+static ssize_t dsi_display_cabc_get(struct device *dev,
+	struct device_attribute *attr, char *buf)
+{
+	struct dsi_display *display;
+	display = dev_get_drvdata(dev);
+	if (!display) {
+		pr_err("Invalid display\n");
+		return snprintf(buf, PAGE_SIZE, "%d\n", 0);
+	}
+
+	return snprintf(buf, PAGE_SIZE, "%d\n", display->panel->cabc_state);
+}
+
+static ssize_t dsi_display_cabc_set(struct device *dev,
+	struct device_attribute *attr, const char *buf, size_t count)
+{
+	struct dsi_display *display;
+	struct msm_param_info param_info;
+	u16 val = 0;
+
+	display = dev_get_drvdata(dev);
+	if (!display) {
+		pr_err("Invalid display\n");
+		return count;
+	}
+
+	if (kstrtou16(buf, 10, &val) < 0)
+		return count;
+
+	param_info.value = val ? CABC_MV_STATE : CABC_UI_STATE;
+	param_info.param_idx = PARAM_CABC_ID;
+	param_info.param_conn_idx = CONNECTOR_PROP_CABC;
+	display->panel->cabc_state = param_info.value;
+
+	if (!dsi_panel_initialized(display->panel))
+		return count;
+
+	if (dsi_panel_set_param(display->panel, &param_info) < 0)
+		pr_err("Failed to set panel parameter id: %d\n", param_info.param_idx);
+
+	return count;
+}
+
 ///sys/devices/platform/soc/soc:qcom,dsi-display/
 static DEVICE_ATTR(dsi_display_early_power, 0644,
 			dsi_display_early_power_read,
@@ -1215,6 +1301,12 @@ static DEVICE_ATTR(dsi_display_parse_para, 0644,
 static DEVICE_ATTR(dsi_display_hbm, 0644,
 			dsi_display_hbm_get,
 			dsi_display_hbm_set);
+static DEVICE_ATTR(dsi_display_acl, 0644,
+			dsi_display_acl_get,
+			dsi_display_acl_set);
+static DEVICE_ATTR(dsi_display_cabc, 0644,
+			dsi_display_cabc_get,
+			dsi_display_cabc_set);
 
 static struct attribute *dsi_display_mot_ext_fs_attrs[] = {
 	&dev_attr_dsi_display_early_power.attr,
@@ -1234,6 +1326,20 @@ static struct attribute *dsi_display_mot_ext_fs_hbm_attrs[] = {
 static struct attribute_group dsi_display_mot_ext_fs_hbm_attrs_group = {
 	.attrs = dsi_display_mot_ext_fs_hbm_attrs,
 };
+static struct attribute *dsi_display_mot_ext_fs_acl_attrs[] = {
+	&dev_attr_dsi_display_acl.attr,
+	NULL,
+};
+static struct attribute_group dsi_display_mot_ext_fs_acl_attrs_group = {
+	.attrs = dsi_display_mot_ext_fs_acl_attrs,
+};
+static struct attribute *dsi_display_mot_ext_fs_cabc_attrs[] = {
+	&dev_attr_dsi_display_cabc.attr,
+	NULL,
+};
+static struct attribute_group dsi_display_mot_ext_fs_cabc_attrs_group = {
+	.attrs = dsi_display_mot_ext_fs_cabc_attrs,
+};
 
 static int dsi_display_sysfs_ext_init(struct dsi_display *display)
 {
@@ -1250,6 +1356,18 @@ static int dsi_display_sysfs_ext_init(struct dsi_display *display)
 		if (rc < 0)
 			pr_err("Failed to create HBM sysfs nodes: %d\n", rc);
 	}
+	if (display->panel->param_cmds[PARAM_ACL_ID].is_supported) {
+		rc = sysfs_create_group(&dev->kobj,
+			&dsi_display_mot_ext_fs_acl_attrs_group);
+		if (rc < 0)
+			pr_err("Failed to create ACL sysfs nodes: %d\n", rc);
+	}
+	if (display->panel->param_cmds[PARAM_CABC_ID].is_supported) {
+		rc = sysfs_create_group(&dev->kobj,
+			&dsi_display_mot_ext_fs_cabc_attrs_group);
+		if (rc < 0)
+			pr_err("Failed to create CABC sysfs nodes: %d\n", rc);
+	}
 
 	return rc;
 }
@@ -1263,6 +1381,12 @@ static int dsi_display_sysfs_ext_deinit(struct dsi_display *display)
 	if (display->panel->param_cmds[PARAM_HBM_ID].is_supported)
 		sysfs_remove_group(&dev->kobj,
 			&dsi_display_mot_ext_fs_hbm_attrs_group);
+	if (display->panel->param_cmds[PARAM_ACL_ID].is_supported)
+		sysfs_remove_group(&dev->kobj,
+			&dsi_display_mot_ext_fs_acl_attrs_group);
+	if (display->panel->param_cmds[PARAM_CABC_ID].is_supported)
+		sysfs_remove_group(&dev->kobj,
+			&dsi_display_mot_ext_fs_cabc_attrs_group);
 
 	return 0;
 }

SGCMarkus/android_kernel_motorola_sm8250

techpack/display/msm/dsi/dsi_display_mot_ext.c
@@ -1198,6 +1198,92 @@ static ssize_t dsi_display_hbm_set(struct device *dev,
 	return count;
 }
 
+static ssize_t dsi_display_acl_get(struct device *dev,
+	struct device_attribute *attr, char *buf)
+{
+	struct dsi_display *display;
+	display = dev_get_drvdata(dev);
+	if (!display) {
+		pr_err("Invalid display\n");
+		return snprintf(buf, PAGE_SIZE, "%d\n", 0);
+	}
+
+	return snprintf(buf, PAGE_SIZE, "%d\n", display->panel->acl_state);
+}
+
+static ssize_t dsi_display_acl_set(struct device *dev,
+	struct device_attribute *attr, const char *buf, size_t count)
+{
+	struct dsi_display *display;
+	struct msm_param_info param_info;
+	u16 val = 0;
+
+	display = dev_get_drvdata(dev);
+	if (!display) {
+		pr_err("Invalid display\n");
+		return count;
+	}
+
+	if (kstrtou16(buf, 10, &val) < 0)
+		return count;
+
+	param_info.value = val ? ACL_ON_STATE : ACL_OFF_STATE;
+	param_info.param_idx = PARAM_ACL_ID;
+	param_info.param_conn_idx = CONNECTOR_PROP_ACL;
+	display->panel->acl_state = param_info.value;
+
+	if (!dsi_panel_initialized(display->panel))
+		return count;
+
+	if (dsi_panel_set_param(display->panel, &param_info) < 0)
+		pr_err("Failed to set panel parameter id: %d\n", param_info.param_idx);
+
+	return count;
+}
+
+static ssize_t dsi_display_cabc_get(struct device *dev,
+	struct device_attribute *attr, char *buf)
+{
+	struct dsi_display *display;
+	display = dev_get_drvdata(dev);
+	if (!display) {
+		pr_err("Invalid display\n");
+		return snprintf(buf, PAGE_SIZE, "%d\n", 0);
+	}
+
+	return snprintf(buf, PAGE_SIZE, "%d\n", display->panel->cabc_state);
+}
+
+static ssize_t dsi_display_cabc_set(struct device *dev,
+	struct device_attribute *attr, const char *buf, size_t count)
+{
+	struct dsi_display *display;
+	struct msm_param_info param_info;
+	u16 val = 0;
+
+	display = dev_get_drvdata(dev);
+	if (!display) {
+		pr_err("Invalid display\n");
+		return count;
+	}
+
+	if (kstrtou16(buf, 10, &val) < 0)
+		return count;
+
+	param_info.value = val ? CABC_MV_STATE : CABC_UI_STATE;
+	param_info.param_idx = PARAM_CABC_ID;
+	param_info.param_conn_idx = CONNECTOR_PROP_CABC;
+	display->panel->cabc_state = param_info.value;
+
+	if (!dsi_panel_initialized(display->panel))
+		return count;
+
+	if (dsi_panel_set_param(display->panel, &param_info) < 0)
+		pr_err("Failed to set panel parameter id: %d\n", param_info.param_idx);
+
+	return count;
+}
+
 ///sys/devices/platform/soc/soc:qcom,dsi-display/
 static DEVICE_ATTR(dsi_display_early_power, 0644,
 			dsi_display_early_power_read,
@@ -1215,6 +1301,12 @@ static DEVICE_ATTR(dsi_display_parse_para, 0644,
 static DEVICE_ATTR(hbm, 0644,
 			dsi_display_hbm_get,
 			dsi_display_hbm_set);
+static DEVICE_ATTR(acl, 0644,
+			dsi_display_acl_get,
+			dsi_display_acl_set);
+static DEVICE_ATTR(cabc, 0644,
+			dsi_display_cabc_get,
+			dsi_display_cabc_set);
 
 static struct attribute *dsi_display_mot_ext_fs_attrs[] = {
 	&dev_attr_dsi_display_early_power.attr,
@@ -1234,6 +1326,20 @@ static struct attribute *dsi_display_mot_ext_fs_hbm_attrs[] = {
 static struct attribute_group dsi_display_mot_ext_fs_hbm_attrs_group = {
 	.attrs = dsi_display_mot_ext_fs_hbm_attrs,
 };
+static struct attribute *dsi_display_mot_ext_fs_acl_attrs[] = {
+	&dev_attr_acl.attr,
+	NULL,
+};
+static struct attribute_group dsi_display_mot_ext_fs_acl_attrs_group = {
+	.attrs = dsi_display_mot_ext_fs_acl_attrs,
+};
+static struct attribute *dsi_display_mot_ext_fs_cabc_attrs[] = {
+	&dev_attr_cabc.attr,
+	NULL,
+};
+static struct attribute_group dsi_display_mot_ext_fs_cabc_attrs_group = {
+	.attrs = dsi_display_mot_ext_fs_cabc_attrs,
+};
 
 static int dsi_display_sysfs_ext_init(struct dsi_display *display)
 {
@@ -1250,6 +1356,18 @@ static int dsi_display_sysfs_ext_init(struct dsi_display *display)
 		if (rc < 0)
 			pr_err("Failed to create HBM sysfs nodes: %d\n", rc);
 	}
+	if (display->panel->param_cmds[PARAM_ACL_ID].is_supported) {
+		rc = sysfs_create_group(&dev->kobj,
+			&dsi_display_mot_ext_fs_acl_attrs_group);
+		if (rc < 0)
+			pr_err("Failed to create ACL sysfs nodes: %d\n", rc);
+	}
+	if (display->panel->param_cmds[PARAM_CABC_ID].is_supported) {
+		rc = sysfs_create_group(&dev->kobj,
+			&dsi_display_mot_ext_fs_cabc_attrs_group);
+		if (rc < 0)
+			pr_err("Failed to create CABC sysfs nodes: %d\n", rc);
+	}
 
 	return rc;
 }
@@ -1263,6 +1381,12 @@ static int dsi_display_sysfs_ext_deinit(struct dsi_display *display)
 	if (display->panel->param_cmds[PARAM_HBM_ID].is_supported)
 		sysfs_remove_group(&dev->kobj,
 			&dsi_display_mot_ext_fs_hbm_attrs_group);
+	if (display->panel->param_cmds[PARAM_ACL_ID].is_supported)
+		sysfs_remove_group(&dev->kobj,
+			&dsi_display_mot_ext_fs_acl_attrs_group);
+	if (display->panel->param_cmds[PARAM_CABC_ID].is_supported)
+		sysfs_remove_group(&dev->kobj,
+			&dsi_display_mot_ext_fs_cabc_attrs_group);
 
 	return 0;
 }